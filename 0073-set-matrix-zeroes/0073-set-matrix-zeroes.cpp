108c591706e2bf4e34700e4fa0937a9d2f67dbcc
riyaasj <riyasebyjohn@gmail.com>
2025-04-30 13:33:04 +0530

    Time: 0 ms (100%), Space: 18.6 MB (79.24%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -11,22 +11,13 @@ public:
                     columns[j] = 1;
                 }
             }
-        }   
+        }
         for(int i = 0; i < m; i++){
-            if(rows[i] == 1){
-                for(int j = 0; j < n; j++){
+            for(int j = 0; j < n; j++){
+                if(rows[i] == 1 || columns[j] == 1){
                     matrix[i][j] = 0;
                 }
             }
-        }
-        
-        for(int i = 0; i < n; i++){
-            if(columns[i] == 1){
-                for(int j = 0; j < m; j++){
-                    matrix[j][i] = 0;
-                }
-            }
-        }
-        
+        } 
     }
 };
