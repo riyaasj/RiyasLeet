585639d442330be538b0382e32644ee8c845fe87
riyaasj <riyasebyjohn@gmail.com>
2025-05-01 13:35:52 +0530

    Time: 0 ms (100%), Space: 30.4 MB (16.79%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> sub1(nums.end() - k % nums.size(), nums.end());
-        vector<int> sub2(nums.begin(), nums.end() - k % nums.size());
-        sub1.insert(sub1.end(), sub2.begin(), sub2.end());
-        nums = sub1;
+        vector<int> r(nums.size());
+        for(int i = 0; i < nums.size(); i++){
+            r[(i + k) % nums.size()] = nums[i];
+        }
+        nums = r;
     }
 };
