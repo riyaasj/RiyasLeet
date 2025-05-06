dd1953ff74608adf2eeaecaea43632684aca5a3f
riyaasj <riyasebyjohn@gmail.com>
2025-05-06 14:29:01 +0530

    Time: 0 ms (100%), Space: 20.4 MB (58.11%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
         for(int i = 0; i < nums.size(); i++){
-            nums[i] = (nums[nums[i]] % 10) * 10 + nums[i];
+            nums[i] = (nums[nums[i]] % nums.size()) * nums.size() + nums[i];
         }
         for(int i = 0; i < nums.size(); i++){
-            nums[i] = nums[i] / 10;
+            nums[i] = nums[i] / nums.size();
         }
         return nums;
     }
