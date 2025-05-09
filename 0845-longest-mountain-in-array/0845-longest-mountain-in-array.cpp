6c5a2c416c3a15f337845fc2e88fd21bf3ca51c5
riyaasj <riyasebyjohn@gmail.com>
2025-05-09 15:39:22 +0530

    Time: 0 ms (100%), Space: 22.3 MB (49.82%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -3,30 +3,19 @@ public:
     int longestMountain(vector<int>& arr) {
         int m = 0;
         for(int i = 1; i < arr.size() - 1; i++){
-            if(arr[i - 1] >= arr[i] || arr[i + 1] >= arr[i]){
-                continue;
-            }
-            int k = i - 2, c = 3;
-            while(k >= 0){
-                if(arr[k] < arr[k + 1]){
+            if(arr[i - 1] < arr[i] && arr[i + 1] < arr[i]){
+                int k = i - 2, c = 3;
+                while(k >= 0 && arr[k] < arr[k + 1]){
                     c++;
+                    k--;
                 }
-                else{
-                    break;
-                }
-                k--;
-            }
-            k = i + 2;
-            while(k < arr.size()){
-                if(arr[k] < arr[k - 1]){
+                k = i + 2;
+                while(k < arr.size() && arr[k] < arr[k - 1]){
                     c++;
+                    k++;
                 }
-                else{
-                    break;
-                }
-                k++;
+                m = max(m, c);
             }
-            m = max(m, c);
         }
         return m;
     }
