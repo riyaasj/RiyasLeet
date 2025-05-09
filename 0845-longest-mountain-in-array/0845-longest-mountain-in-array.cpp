3443c24582c2fe288062ee42686e7cc27bc4dba7
riyaasj <riyasebyjohn@gmail.com>
2025-05-09 15:33:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        int m = 0;
+        for(int i = 1; i < arr.size() - 1; i++){
+            if(arr[i - 1] >= arr[i] || arr[i + 1] >= arr[i]){
+                continue;
+            }
+            int k = i - 2, c = 3;
+            while(k > 0){
+                if(arr[k] < arr[k + 1]){
+                    c++;
+                }
+                else{
+                    break;
+                }
+                k--;
+            }
+            k = i + 2;
+            while(k < arr.size()){
+                if(arr[k] < arr[k - 1]){
+                    c++;
+                }
+                else{
+                    break;
+                }
+                k++;
+            }
+            m = max(m, c);
+        }
+        return m;
+    }
+};
