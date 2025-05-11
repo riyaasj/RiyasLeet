27e64ecfe7c7abee77bec9525e6227df6afb4f91
riyaasj <riyasebyjohn@gmail.com>
2025-05-11 15:43:00 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,21 +1,25 @@
 class Solution {
+private:
+    void quickSort(vector<int> &nums, int low, int high){
+        if(low < high){
+            int pivot = partition(nums, low, high);
+            quickSort(nums, low, pivot - 1);
+            quickSort(nums, pivot + 1, high);
+        }
+    }
+    int partition(vector<int> &nums, int low, int high){
+        int pivot = nums[high];
+        int i = low - 1;
+        for(int j = low; j <= high - 1; j++){
+            if(nums[j] < pivot){
+                swap(nums[++i], nums[j]);
+            }
+        }
+        swap(nums[++i], nums[high]);
+        return i;
+    }
 public:
     void sortColors(vector<int>& nums) {
-        unordered_map<int, int> colours;
-        for(int i: nums){
-            colours[i]++;
-        }
-        int i = 0;
-        while(i < nums.size()){
-            for(int j = 0; j < colours[0]; j++){
-                nums[i++] = 0;
-            }
-            for(int j = 0; j < colours[1]; j++){
-                nums[i++] = 1;
-            }
-            for(int j = 0; j < colours[2]; j++){
-                nums[i++] = 2;
-            }
-        }
+        quickSort(nums, 0, nums.size() - 1);
     }
 };
