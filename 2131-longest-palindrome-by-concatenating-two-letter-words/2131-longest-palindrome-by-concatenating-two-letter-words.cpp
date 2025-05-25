de8bc5f07b60557c43e7558d0f8db660a621f783
Riya Seby John <riyasebyjohn@gmail.com>
2025-05-25 14:48:46 +0530

    Time: 117 ms (37.86%), Space: 188.8 MB (8.23%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -4,20 +4,15 @@ public:
         unordered_map<string, int> s;
         int c = 0, d = 0;
         for(int i = 0; i < words.size(); i++){
-            if(s.find({words[i][1], words[i][0]}) != s.end()){
+            if(s[{words[i][1], words[i][0]}]){
                 c += 4;
                 if(--s[{words[i][1], words[i][0]}] == 0){
                     s.erase({words[i][1], words[i][0]});
                 }
-
             }
             else{
                 s[words[i]]++;
             }
-            for(auto x: s){
-                cout << x.first << " " << x.second << " ";
-            }
-            cout << "\n";
         }
         for(auto x: s){
             if(x.first[0] == x.first[1]){
