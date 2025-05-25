c2759cdb970b7b74b7bdb0c7d10490e6c46dc9e9
Riya Seby John <riyasebyjohn@gmail.com>
2025-05-25 14:52:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int longestPalindrome(vector<string>& words){
         unordered_map<string, int> s;
-        int c = 0, d = 0;
+        int c = 0;
         for(auto word: words){
             if(s[{word[1], word[0]}]){
                 c += 4;
-                if(--s[{word[1], word[0]}] == 0){
-                    s.erase({word[1], word[0]});
-                }
+                s[{word[1], word[0]}]--;
             }
             else{
                 s[word]++;
@@ -16,10 +14,10 @@ public:
         }
         for(auto x: s){
             if(x.first[0] == x.first[1]){
-                d = 2;
+                c += 2;
                 break;
             }
         }
-        return d + c;
+        return c;
     }
 };
