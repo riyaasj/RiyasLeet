5859c697c498a88f63071a6f2e47653280263071
Riya Seby John <riyasebyjohn@gmail.com>
2025-05-26 19:17:32 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,22 +3,13 @@ public:
     bool isAnagram(string s, string t) {
         unordered_map <char, int> x;
         for(char i: s){
-            if(x.contains(i)){
-                x[i]++;
-            }
-            x.insert({i, 1});
+            x[i]++;
         }
         for(char i: t){
-            if(!x.contains(i)){
-                return false;
+            x[i]--;
+            if(x[i] == 0){
+                x.erase(i);
             }
-            else{
-                x[i]--;
-                if(x[i] == 0){
-                    x.erase(i);
-                }
-            }
-            
         }
         if(x.empty()){
             return true;
