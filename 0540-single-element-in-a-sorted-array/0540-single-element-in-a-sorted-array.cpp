5e46a8f869ce9935c77d6ef87fb935761bbb5341
Riya Seby John <riyasebyjohn@gmail.com>
2025-05-27 15:52:16 +0530

    Time: 0 ms (100%), Space: 26.1 MB (91.66%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,28 +6,28 @@ public:
         }
         int l = 0, r = nums.size() - 1;
         while(l <= r){
-            int m = (l + r) / 2;
-            if(m % 2 == 0){
-                if(m == 0 || m == nums.size() - 1){
-                    return nums[m];
-                }
-                if(nums[m - 1] == nums[m]){
-                    r = m - 1;
-                }
-                else if(nums[m + 1] == nums[m]){
-                    l = m + 1;
+            int m = l + (r - l) / 2;
+            if(m == 0 || m == nums.size() - 1){
+                return nums[m];
+            }
+            else if(nums[m] == nums[m - 1]){
+                if((m - 2) % 2 == 0){
+                    r = m - 2;
                 }
                 else{
-                    return nums[m];
+                    l = m + 1;
+                }
+            }
+            else if(nums[m] == nums[m + 1]){
+                if((m - 1) % 2 == 0){
+                    r = m - 1;
+                }
+                else{
+                    l = m + 2;
                 }
             }
             else{
-                if(nums[m - 1] == nums[m]){
-                    l = m + 1;
-                }
-                else{
-                    r = m - 1;
-                }
+                return nums[m];
             }
         }
         return -1;
