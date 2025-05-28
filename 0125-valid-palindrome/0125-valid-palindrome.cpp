02192afa53d8caa39ea6e4365b232ea17c580ed1
Riya Seby John <riyasebyjohn@gmail.com>
2025-05-28 21:34:50 +0530

    Time: 4 ms (17.28%), Space: 10.3 MB (18.72%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        string res = "";
+        while(l < s.size()){
+            if(isalnum(s[l])){
+                res += tolower(s[l]);
+            }
+            l++;
+        }
+        cout << res;
+        l = 0;
+        int r = res.size() - 1;
+        while(l < r){
+            if(res[l] != res[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
