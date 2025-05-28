387b1885be6f95e0b8b7164b691cf4a6f4408913
Riya Seby John <riyasebyjohn@gmail.com>
2025-05-28 21:47:49 +0530

    Time: 4 ms (17.28%), Space: 9.8 MB (79.1%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int l = 0;
-        string res = "";
-        while(l < s.size()){
-            if(isalnum(s[l])){
-                res += tolower(s[l]);
-            }
-            l++;
-        }
-        l = 0;
-        int r = res.size() - 1;
+        int l = 0, r = s.size() - 1;
         while(l < r){
-            if(res[l] != res[r]){
-                return false;
-            }
+            while(l < s.size() && !isalnum(s[l])) l++;
+            while(r >= 0 && !isalnum(s[r])) r--;
+            if(l < s.size() && r >= 0 && tolower(s[l]) != tolower(s[r])) return false;
             l++;
             r--;
         }
